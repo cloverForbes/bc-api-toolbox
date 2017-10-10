b434e8d4400e902b4f912844b54fb079fb4a48c3
McKinley <solidaritydesignsva@gmail.com>
2017-10-10 01:19:54 -0500

    Added function to Get Number of products and A second to return an array of product IDs

---
+++ toolbox.js
@@ -0,0 +1,46 @@
+const request = require('request-promise');
+
+class Toolbox{
+    constructor(key,id,hash){
+        this.hash = hash;
+        this.options = {
+            url: `https://api.bigcommerce.com/stores/${hash}/v3/`,
+            headers: {
+                'X-Auth-Client': id,
+                'X-Auth-Token': key,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }
+    }
+
+    getTotalNumber(callback){
+        this.options.url = `https://api.bigcommerce.com/stores/${this.hash}/v3/catalog/products`;
+        request.get(this.options, (err,res,body) => {
+            const data = body;
+            callback(JSON.parse(data).meta.pagination.total);
+        })
+    };
+
+    getProductIds(callback){
+        this.getTotalNumber(number => {
+            let ids = [];
+            for(let x=0;x<=number/50;x++){
+                this.options.url = `https://api.bigcommerce.com/stores/${this.hash}/v3/catalog/products?page=${x+1}&limit=50`;
+                request.get(this.options, (err,res,body) => {
+                    const data = JSON.parse(body).data;
+                    data.forEach(item  => {
+                        ids.push(item.id);
+                    });
+                    if(ids.length === number){
+                        callback(ids);
+                    }
+                })
+            }
+        })
+    }
+}
+
+
+
+module.exports = Toolbox;
